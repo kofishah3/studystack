010d5c1f60dc453b5575502f3c948e04cdb8f644
JohSANCoding <129604435+JohSANCoding@users.noreply.github.com>
2026-04-29 19:41:51 +0800

    refactor(types): export branded ID types from db.ts

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,9 +1,9 @@
-type UserID        = string & { readonly _brand: 'UserID' };
-type QuestionID    = string & { readonly _brand: 'QuestionID' };
-type TutorialID    = string & { readonly _brand: 'TutorialID' };
-type AnswerID      = number & { readonly _brand: 'AnswerID' };
-type CommentID     = number & { readonly _brand: 'CommentID' };
-type InteractionID = number & { readonly _brand: 'InteractionID' };
+export type UserID        = string & { readonly _brand: 'UserID' };
+export type QuestionID    = string & { readonly _brand: 'QuestionID' };
+export type TutorialID    = string & { readonly _brand: 'TutorialID' };
+export type AnswerID      = number & { readonly _brand: 'AnswerID' };
+export type CommentID     = number & { readonly _brand: 'CommentID' };
+export type InteractionID = number & { readonly _brand: 'InteractionID' };
 
 
 export interface User {
