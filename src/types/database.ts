c716d8b6f2cd6322c0d8ea3d1e695ebd0035a251
JohSANCoding <129604435+JohSANCoding@users.noreply.github.com>
2026-05-03 13:14:41 +0800

    chore: small format changes

--- src/types/database.ts
+++ src/types/database.ts
@@ -4,9 +4,7 @@ export type TutorialID = string & { readonly _brand: "TutorialID" };
 export type AnswerID = number & { readonly _brand: "AnswerID" };
 export type CommentID = number & { readonly _brand: "CommentID" };
 export type InteractionID = number & { readonly _brand: "InteractionID" };
-export type TutorialMaterialID = string & {
-  readonly _brand: "TutorialMaterialID";
-};
+export type TutorialMaterialID = string & { readonly _brand: "TutorialMaterialID";};
 
 export interface User {
   user_id: UserID;
