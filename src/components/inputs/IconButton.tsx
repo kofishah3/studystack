cf2695e8c4661ce9650ce1a6fde80fbfaebd4a19
Ishah Bautista <kofishah3@gmail.com>
2026-05-04 16:11:10 +0800

    feat: added 5 new seeded accounts for testing, all have posts, comments, answers, etc.

---
+++ src/components/inputs/IconButton.tsx
@@ -0,0 +1,32 @@
+import { LucideIcon } from "lucide-react";
+
+interface IconButtonProps {
+  label?: string;
+  icon: LucideIcon;
+  iconColor: string;
+  labelColor?: string;
+  hoverBg?: string;
+  iconSize?: number;
+  onClick?: () => void;
+}
+
+export default function IconButton({
+  label,
+  icon: Icon,
+  iconColor,
+  labelColor = iconColor,
+  hoverBg = "hover:bg-primary-100/40",
+  iconSize = 14,
+  onClick,
+}: IconButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`text-${labelColor} p-2 rounded-full text-xs
+      ${hoverBg} transition-all duration-200 cursor-pointer flex items-center gap-2`}
+    >
+      <Icon size={iconSize} />
+      {label && <span>{label}</span>}
+    </button>
+  );
+}
